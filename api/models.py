9f3a2c43d6fc9b664bf1b36be2dcdecc42658099
IslaMurtazaev <islam.muratazaev@gmail.com>
2018-11-26 21:10:36 +0600

    Add user and message models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    is_bot = models.BooleanField()
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    text = models.TextField(max_length=4096)
