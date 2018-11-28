066cfbe9845de9c5ffcded5af95647753409284e
IslaMurtazaev <islam.muratazaev@gmail.com>
2018-11-28 23:45:33 +0600

    Add BOT_TOKEN to settings.py

--- main/settings.py
+++ main/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
